cceb3da08955456299fb11d2ce421e5fc830a8cb
fernandomaseda <fernando.maseda@comunidad.ub.edu.ar>
2023-01-26 17:18:36 -0300

    clean

--- next-sitemap.config.js
+++
@@ -1,5 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-    siteUrl: 'https://www.develophard.com',
-  generateRobotsTxt: true,
-  }
--- pages/posts.tsx
+++ pages/posts.tsx
@@ -30,7 +30,7 @@ const Posts = () => {
   useEffect(() => {
     const callApi = async () => {
       try {
-        const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+        const response = await fetch(process.env.NEXT_PUBLIC_API_URL + '/posts');
         const data = await response.json();
         if (response.ok && 'userId' in data[0]) setState({ ...state, data: [...data.slice(0, 5)] });
       } catch (error) {
